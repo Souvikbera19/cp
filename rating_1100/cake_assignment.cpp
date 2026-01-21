ce67bcd7a4500dbbef8786d00f22dab113dc8965
SOUVIK BERA <berasouvik616@gmail.com>
2026-01-21 22:25:49 +0530

    used backtracking

--- rating_1100/cake_assignment.cpp
+++ rating_1100/cake_assignment.cpp
@@ -1,7 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+void solve(){
+    ll k,x;
+    cin >> k >> x;
+    ll total_cake = 1ll<<k;
+    if(x==pow(2,k)){
+        cout << 0 << '\n';
+        return;
+    }
 
+    ll y = 2*total_cake-x;
+    vector<ll>ans;
+    while(x!=total_cake){
+        if(x>y){
+            ans.push_back(2);
+            y*=2;
+            x=2*total_cake-y;
+        }
+        else if(x<y){
+            ans.push_back(1);
+            x*=2;
+            y=2*total_cake-x;
+        }
+    }
+    reverse(ans.begin(),ans.end());
+    cout << ans.size() << '\n';
+    for(auto it:ans) cout << it << ' ';
+    cout << '\n';
+
+}
 int main() {
 
 
@@ -11,7 +39,7 @@ int main() {
     int t;
     cin >> t;
     while(t--){
-        ll k,x;
+        solve();
         
     }
 
